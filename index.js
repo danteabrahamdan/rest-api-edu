8dfcaffa548d106d9de653eb66ca56490ddf7939
Dante Abraham Quispe <dquispe@innovapplications.com>
2021-03-18 19:53:44 -0400

    version 0.9.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
@@ -13,4 +14,7 @@ app.use('/api/usuarios', require('./routes/usuarios-routes'));
 app.use('/api/hospitales', require('./routes/hospitales-routes'));
 app.use('/api/medicos', require('./routes/medicos-routes'));
 app.use('/api/login', require('./routes/auth-routes'));
+app.use('/api/upload', require('./routes/uploads-routes'));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/index.html')));
+
 app.listen(process.env.PORT, () => console.log(`Servidor en el puerto ${process.env.PORT}`));
